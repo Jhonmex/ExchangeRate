a08dfbdebacb454c0317f6289c71f5dad127e7f9
Jhonmex <financejhon@gmail.com>
2024-05-07 16:47:29 -0600

    Nuevo metodo y se eliminan variables

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,17 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Moneda moneda = new Moneda();
         System.out.println("*******************************************");
         System.out.println("Bienvenidos al conversor de monedas");
 
-        System.out.println("1- Dolar => Peso Argentino");
-        System.out.println("2- Peso Argentino => Dolar");
-        System.out.println("3- Dolar => Real Brasileño");
-        System.out.println("4- Real Brasileño => Dolar");
-        System.out.println("5- Dolar => Peso Colombiano");
-        System.out.println("6- Peso Colombiano => Dolar");
-        System.out.println("7- Salir");
-        System.out.println("Elija una opcion valida");
-        Moneda moneda = new Moneda();
+        System.out.println("Escribe la moneda base");
+        moneda.currency = sc.nextLine();
+
         moneda.realizarSolicitud();
 
 
