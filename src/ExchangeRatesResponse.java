74bdf471215d52a51ea9fb49b59b20f85473d123
Jhonmex <financejhon@gmail.com>
2024-05-09 11:25:05 -0600

    Update ExchangeRatesResponse.java

--- src/ExchangeRatesResponse.java
+++ src/ExchangeRatesResponse.java
@@ -78,9 +78,7 @@ public class ExchangeRatesResponse {
     public Map<String, Double> getConversion_rates() {
         return conversion_rates;
     }
-
-
-
+    
     public void setConversion_rates(Map<String, Double> conversion_rates) {
         this.conversion_rates = conversion_rates;
     }
